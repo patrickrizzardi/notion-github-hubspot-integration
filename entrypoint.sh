d1ab2c14023b89d83232deaba3fa96a023981a85
Patrick Rizzardi <35274364+RedactDigital@users.noreply.github.com>
2024-05-01 13:00:09 -0400

    [Wed May  1 13:00:09 EDT 2024] finalize build flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,6 @@
 PATH_SECRETS=/run/secrets
 EXISTING_ENV_FILE=$WORKDIR/.env
 
-echo $EXISTING_ENV_FILE
-
-echo "WORKDIR ownership:" $(ls -ld $WORKDIR)
-
-echo $USER
-
 # If the .env file exists, stop the script and tell the user to remove it and try again
 # This way if we are testing the script, we don't accidentally overwrite the .env file
 if [ -f "$EXISTING_ENV_FILE" ]; then
